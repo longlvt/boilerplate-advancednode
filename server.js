15c1f68c785f9c1ab5a3bd212d4e9ff67d62bbd9
Long LE <gop_long@m1.com.sg>
2022-12-19 21:18:16 +0800

    revert

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ myDB(async (client) => {
   const myDataBase = await client.db('database').collection('users');
   
   passport.use(new LocalStrategy(
-    function(username, password, doneaaa) {
+    function(username, password, done) {
       myDataBase.findOne({ username: username }, function (err, user) {
         console.log('User '+ username +' attempted to log in.');
         if (err) { return done(err); }
