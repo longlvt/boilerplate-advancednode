3b949ecf548affed6ae3c3300cf89683aea23017
Long LE <gop_long@m1.com.sg>
2023-02-06 23:32:21 +0800

    add register logic

--- server.js
+++ server.js
@@ -40,13 +40,46 @@ myDB(async (client) => {
     res.render('pug', {
       title: 'Connected to Database',
       message: 'Please login',
-      showLogin: true
+      showLogin: true,
+      showRegistration: true
     });
   });
 
   app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
     res.render('/profile')
   });
+
+  app.route('/register')
+  .post((req, res, next) => {
+    myDataBase.findOne({ username: req.body.username }, (err, user) => {
+      if (err) {
+        console.log('CAN NOT FIND USER');
+        next(err);
+      } else if (user) {
+        res.redirect('/');
+      } else {
+        myDataBase.insertOne({
+          username: req.body.username,
+          password: req.body.password
+        },
+          (err, doc) => {
+            if (err) {
+              res.redirect('/');
+            } else {
+              // The inserted document is held within
+              // the ops property of the doc
+              next(null, doc.ops[0]);
+            }
+          }
+        )
+      }
+    })
+  },
+    passport.authenticate('local', { failureRedirect: '/' }),
+    (req, res, next) => {
+      res.redirect('/profile');
+    }
+  );
 
   app.route('/profile').get(ensureAuthenticated, (req, res) => {
     console.log
@@ -68,8 +101,11 @@ myDB(async (client) => {
       myDataBase.findOne({ username: username }, function (err, user) {
         console.log('User '+ username +' attempted to log in.');
         if (err) { return done(err); }
-        if (!user) { return done(null, false); }
-        if (password !== user.password) { return done(null, false); }
+        if (!user) { console.log('NOT USER');return done(null, false); }
+        if (password !== user.password) {
+          console.log('PASSWORD NOT MATCHED');
+          return done(null, false);
+        }
         return done(null, user);
       });
     }
