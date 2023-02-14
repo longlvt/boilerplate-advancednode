21f06fd796524310ff1b5a99f2ecc40bf6f1bd07
Long LE <gop_long@m1.com.sg>
2023-02-14 12:44:54 +0800

    add github routes

--- routes.js
+++ routes.js
@@ -16,54 +16,63 @@ module.exports = function (app, myDataBase) {
           title: 'Connected to Database',
           message: 'Please login',
           showLogin: true,
-          showRegistration: true
+          showRegistration: true,
+          showSocialAuth: true
         });
       });
     
-      app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
+    app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
         res.render('/profile')
-      });
+    });
     
-      app.route('/register')
-      .post((req, res, next) => {
-        myDataBase.findOne({ username: req.body.username }, (err, user) => {
-          if (err) {
-            console.log('CAN NOT FIND USER');
-            next(err);
-          } else if (user) {
-            res.redirect('/');
-          } else {
-            myDataBase.insertOne({
-              username: req.body.username,
-              password: bcrypt.hashSync(req.body.password, 12),
-            },
-              (err, doc) => {
-                if (err) {
-                  res.redirect('/');
-                } else {
-                  // The inserted document is held within
-                  // the ops property of the doc
-                  next(null, doc.ops[0]);
-                }
-              }
-            )
-          }
-        })
-      },
-        passport.authenticate('local', { failureRedirect: '/' }),
-        (req, res, next) => {
-          res.redirect('/profile');
+    app.route('/auth/github').get(passport.authenticate('github'), (req, res) => {
+        res.render('/profile')
+    });
+    
+    app.route('/auth/github/callback').get(passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
+        res.render('/profile')
+    });
+
+    app.route('/register')
+        .post((req, res, next) => {
+    myDataBase.findOne({ username: req.body.username }, (err, user) => {
+        if (err) {
+        console.log('CAN NOT FIND USER');
+        next(err);
+        } else if (user) {
+        res.redirect('/');
+        } else {
+        myDataBase.insertOne({
+            username: req.body.username,
+            password: bcrypt.hashSync(req.body.password, 12),
+        },
+            (err, doc) => {
+            if (err) {
+                res.redirect('/');
+            } else {
+                // The inserted document is held within
+                // the ops property of the doc
+                next(null, doc.ops[0]);
+            }
+            }
+        )
         }
-      );
-    
-      app.route('/profile').get(ensureAuthenticated, (req, res) => {
+    })
+    },
+    passport.authenticate('local', { failureRedirect: '/' }),
+    (req, res, next) => {
+        res.redirect('/profile');
+    }
+    );
+
+    app.route('/profile').get(ensureAuthenticated, (req, res) => {
         console.log
         res.render('profile', {
-          username: req.user.username,
+            username: req.user.username,
         });
-      });
-    
-      app.route('/logout').get((req, res) => {
+    });
+
+    app.route('/logout').get((req, res) => {
         req.logout(); res.redirect('/');
-      });
+    });
 }
