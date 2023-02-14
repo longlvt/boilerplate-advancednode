e21d3f9109a467ab8140d7b2eb416f11cfed39e5
Long LE <gop_long@m1.com.sg>
2023-02-14 14:04:36 +0800

    add database updating when logging with Github account

--- auth.js
+++ auth.js
@@ -29,8 +29,32 @@ module.exports = function (app, myDataBase) {
         callbackURL: process.env.GITHUB_CLIENT_CALLBACK_URL
       },
         function(accessToken, refreshToken, profile, cb) {
-            console.log(profile);
-            //Database logic here with callback containing your user object
+            myDataBase.findOneAndUpdate(
+                { id: profile.id },
+                {
+                  $setOnInsert: {
+                    id: profile.id,
+                    username: profile.username,
+                    name: profile.displayName || 'John Doe',
+                    photo: profile.photos[0].value || '',
+                    email: Array.isArray(profile.emails)
+                      ? profile.emails[0].value
+                      : 'No public email',
+                    created_on: new Date(),
+                    provider: profile.provider || ''
+                  },
+                  $set: {
+                    last_login: new Date()
+                  },
+                  $inc: {
+                    login_count: 1
+                  }
+                },
+                { upsert: true, new: true },
+                (err, doc) => {
+                  return cb(null, doc.value);
+                }
+            );
         }
       ));
 
@@ -41,7 +65,7 @@ module.exports = function (app, myDataBase) {
     
     passport.deserializeUser((id, done) => {
         myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-        done(null, doc);
+            done(null, doc);
         });
     });
 }
