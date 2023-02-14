f8034a38afa4740c281a7a7efc74d1895cb27b4f
Long LE <gop_long@m1.com.sg>
2023-02-14 15:22:15 +0800

    add disconnect handle, authentication with socket.io

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const routes = require('./routes.js');
 const auth = require('./auth.js');
 
+const passportSocketIo = require('passport.socketio');
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo')(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
+
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
@@ -16,12 +22,23 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
-  cookie: { secure: false }
-}));
+// app.use(session({
+//   secret: process.env.SESSION_SECRET,
+//   resave: true,
+//   saveUninitialized: true,
+//   cookie: { secure: false }
+// }));
+  
+io.use(
+  passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    key: 'express.sid',
+    secret: process.env.SESSION_SECRET,
+    store: store,
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail
+  })
+);
 
 myDB(async (client) => {
   const myDataBase = await client.db('database').collection('users');
@@ -31,10 +48,25 @@ myDB(async (client) => {
   let currentUsers = 0;
   io.on('connection', socket => {
     console.log('A user has connected');
-    ++currentUsers;
-    io.emit('user count', currentUsers);
-  });
+    console.log('user ' + socket.request.user.username + ' connected');
 
+    ++currentUsers;
+    io.emit('user', {
+      username: socket.request.user.username,
+      currentUsers,
+      connected: true
+    });
+
+    socket.on('disconnect', () => {
+      console.log('A user has dis-connected');
+      --currentUsers;
+      io.emit('user', {
+        username: socket.request.user.username,
+        currentUsers,
+        connected: false
+      });
+    });
+  });
 }).catch((e) => {
   app.route('/').get((req, res) => {
     res.render(process.cwd() + '/views/pug/index', 
@@ -47,3 +79,15 @@ const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
 });
+
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
+
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
